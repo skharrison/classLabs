8631563dd756ef1118ab5552ca691116baa2da1d
skharrison <sharr199@uncc.edu>
2020-12-03 22:27:16 -0500

    removed not necessary code in fastaSequence

--- src/lab7/FastaSequence.java
+++ src/lab7/FastaSequence.java
@@ -9,7 +9,6 @@ import java.util.ArrayList;
 import java.util.Map;
 import java.io.BufferedReader;
 
-
 public class FastaSequence {
 	
 	private final String header;
@@ -74,105 +73,4 @@ public class FastaSequence {
 		return(fastaList);
 		
 	}
-	
-	// takes fasta list and map where k is sequence and value is SeqCount object
-	// SeqCount object increments count if comes across sequence
-//	public static void writeUnique(String outFile, List<FastaSequence> fastaList) throws Exception 
-//	{
-//		Map<Integer, SeqCount> counter = new HashMap<Integer, SeqCount>();
-//		
-//		for (FastaSequence fastSeq : fastaList)
-//		{
-//			//Saving string as hashKey in map to save memory if large seqs
-//			String sequence = fastSeq.sequence;
-//			int hashKey = sequence.hashCode();
-//			if (counter.containsKey(hashKey)) 
-//			{
-//				SeqCount count = counter.get(hashKey);
-//				count.incrementCount();
-//			}
-//			else 
-//			{
-//				SeqCount count = new SeqCount(sequence, 1);
-//				counter.put(hashKey, count);
-//			}
-//		}
-//		
-//		List<SeqCount> sortSeqCount = sortSeqs(counter);
-//		SeqCount.writeCountFasta(sortSeqCount, outFile);    // calls static SeqCount method to make new count fasta
-//		
-//	}
-//	
-//	// adds each SeqCount object to a list and returns sorted list based on comparable of SeqCount
-//	public static List<SeqCount> sortSeqs(Map<Integer, SeqCount> counter)
-//	{
-//		List<SeqCount> seqC = new ArrayList<SeqCount>();
-//		for (SeqCount entry : counter.values())
-//		{
-//			seqC.add(entry);
-//		}
-//		
-//		Collections.sort(seqC);
-//		return(seqC);
-//		
-//	}
-//	
-//	public String getHeader()
-//	{
-//		return(this.header);
-//	}
-//	
-//	public String getSequence()
-//	{
-//		return(this.sequence);
-//	}
-//	
-//	public float getGCRatio()
-//	{
-//		return(this.gcRatio);
-//	}
-//	
-//	public void computeGC()
-//	{
-//		int gcCount = 0;
-//		
-//		String[] seqs = this.sequence.split("");
-//		
-//		for (String s : seqs)
-//		{
-//			if (s.equals("G") || s.equals("C"))
-//			{
-//				gcCount++;
-//			}
-//		}
-//		
-//		float gcRatio = (float) gcCount / seqs.length;
-//		this.gcRatio = gcRatio;
-//		
-//	}
-//	
-//	@Override
-//	public String toString()
-//	{
-//		return String.format("Fasta Sequence with ID: " + this.header);
-//	}
-//	
-//	public static void main(String[] args) throws Exception
-//	{
-//		String fileIn = "/home/sarah/school/adv_program/uniqueTest.fasta";
-//		String fileOut = "/home/sarah/school/adv_program/uniqueTestOut.fasta";
-//		
-//	
-//		List<FastaSequence> fastaList = FastaSequence.readFastaFile(fileIn);
-//		
-//		for(FastaSequence fs : fastaList)
-//		{
-//			fs.computeGC();
-//			System.out.println(fs.getHeader());
-//			System.out.println(fs.getSequence());
-//			System.out.println(fs.getGCRatio());
-//		}
-//
-//		writeUnique(fileOut, fastaList);
-//	}
 }
